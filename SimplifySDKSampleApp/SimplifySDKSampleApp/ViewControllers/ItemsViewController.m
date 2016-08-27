2a9e43c37fc8c9e6d3a842e2b681dd38635e9333
Kevin Li <kevinli120@yahoo.com>
2016-08-27 15:58:36 -0700

    CollectionView items
    Formatted CollectionView(Cell), no data transferred in segue yet

--- SimplifySDKSampleApp/SimplifySDKSampleApp/ViewControllers/ItemsViewController.m
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/ViewControllers/ItemsViewController.m
@@ -6,11 +6,12 @@
 //  Copyright © 2016 MasterCard. All rights reserved.
 //
 
+#import "ItemCollectionViewCell.h"
 #import "ItemsViewController.h"
 
 @interface ItemsViewController ()
 
-@property (weak, nonatomic) IBOutlet UICollectionView *_itemsCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *itemsCollectionView;
 
 @end
 
@@ -20,10 +21,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [__itemsCollectionView setDelegate:self];
-    [__itemsCollectionView setDataSource:self];
+    self.automaticallyAdjustsScrollViewInsets = false;
     
-    [__itemsCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"ItemCell"];
+    [_itemsCollectionView setDelegate:self];
+    [_itemsCollectionView setDataSource:self];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,33 +32,44 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Collection View
+#pragma mark - Collection View Data
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 10;
+    return 20;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     // Placeholder cell for items
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ItemCell" forIndexPath:indexPath];
+    ItemCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ItemCell" forIndexPath:indexPath];
     cell.backgroundColor = [UIColor greenColor];
     return cell;
 }
 
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    return CGSizeMake(50, 50);
+#pragma mark - Collection View Layout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSInteger screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    
+    // Half the width minus margins
+    return CGSizeMake((screenWidth - 30) / 2, (screenWidth - 30) / 2);
+}
+
+#pragma mark - Collection View Selection
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    [self performSegueWithIdentifier:@"ShowProduct" sender:self];
 }
 
 
-/*
  #pragma mark - Navigation
  
  // In a storyboard-based application, you will often want to do a little preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
  // Get the new view controller using [segue destinationViewController].
  // Pass the selected object to the new view controller.
+     
  }
- */
+ 
 
 @end
