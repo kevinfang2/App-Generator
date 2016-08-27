4cb9c85490e48f3d23f8a43759665ad711105eeb
kevinfang2 <kevkev0123@gmail.com>
2016-08-27 10:42:50 -0700

    asdfasdf

---
+++ bank thing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bank thing
+//
+//  Created by Kevin Fang on 8/27/16.
+//  Copyright © 2016 Kevin Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
