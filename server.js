2276cc6b0725aeb6ffd62a2e3c259971e14940fb
myh1000 <myh1000@gmail.com>
2016-08-27 22:56:03 -0700

    Good Code AUTOCOMPLETE

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var path = require('path');
 var fs = require('fs');
 var app = express();
+var firebase = require('firebase');
 
 app.set('view engine', 'ejs');
 
@@ -17,13 +18,38 @@ app.get('/products', function(req, res) {
  	// res.render('products.ejs', {root: __dirname})
 });
 
+String.prototype.format = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
 function data() {
-	return '[titles addObject:@"memsssse"];';
+	var myFirebaseRef = new firebase("https://bankhackathon.firebaseio.com/");
+	var dataArray = "";
+	var retString = "";
+	console.log("MEM");
+	var ret = ""
+	myFirebaseRef.on("value", function(snapshot) {
+		var data = snapshot.val().items;
+		dataArray = Object.keys(data).map(function(k) { return data[k] });
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+	});
+	for (i in dataArray) {
+		var item = dataArray[i];
+		var str = '[titles addObject:@"{0}"];\n\t[description addObject:@"{1}"];\n\t[images addObject:@"{2}"];'.format(item.Name, item.Description, item.image);
+		retString += str;
+	}
+	return retString;
 }
 
 app.post('/submit', function (req, res) {
     console.log('submit pressed');
-    console.log(data());
 	var populationData = data()
     fs.readFile("SimplifySDKSampleApp/SimplifySDKSampleApp/ViewControllers/ItemsViewController.m", 'utf8', function (err,data) {
         if (err) {
