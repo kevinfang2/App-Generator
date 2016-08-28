652d682b1b3ce7b34cea93bfbafa9c400538674d
myh1000 <myh1000@gmail.com>
2016-08-27 18:16:15 -0700

    beta autocode

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -15,6 +15,26 @@ app.get('/', function (req, res) {
 app.get('/products', function(req, res) {
 	res.render('pages/products');
  	// res.render('products.ejs', {root: __dirname})
+});
+
+function data() {
+	return "mem";
+}
+
+app.post('/submit', function (req, res) {
+    console.log('submit pressed');
+    console.log(data());
+	var view = data()
+    fs.readFile("test", 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/\/\/string to be replaced/g, view);
+
+        fs.writeFile("testcopy", result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
 });
 
 app.listen(3141)
